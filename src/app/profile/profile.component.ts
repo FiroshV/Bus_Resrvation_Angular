f04a3cf16723c76bd3599cc09489bd3d2e9da036
sreenija <sreenija4363@gmail.com>
2021-10-02 20:46:31 +0530

    seats completed profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -13,21 +13,23 @@ export class ProfileComponent implements OnInit {
 
   profile!: Profile;
 var1: boolean=false;
+gender!:boolean;
   constructor(private service:ProfileService,private router:Router) { }
 
   ngOnInit(): void {
     this.profile=JSON.parse(sessionStorage.getItem("user") || "{}");
+    this.gender=this.getGender();
   }
   
   onShow()
   {
-    console.log("gfvgbhnj");
+    
 this.router.navigate(['/changePassword'])
 
   }
 
   getGender(){
-    return JSON.parse(sessionStorage.getItem('user')||"male").gender=="male";
+    return JSON.parse(sessionStorage.getItem('user')||"M").gender=="M";
   }
   
 }
